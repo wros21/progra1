928a4a89192c3a88261a0f7d0a280a0215d4e683
wros21 <34988061+wros21@users.noreply.github.com>
2022-02-17 13:47:39 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+    printf ("             __.-/|\n");
+    printf ("             %c'o_O'\n",92);
+    printf ("              =( )=  +---------+\n");
+    printf ("                U|   | Wilhelm |\n");
+    printf ("      /%c  /%c   / |   +---------+\n",92,92);
+    printf ("     ) /^%c) ^%c/ _)%c     |\n",92,92,92);
+    printf ("     )   /^%c/   _) %c    |\n",92,92);
+    printf ("     )   _ /  / _)  %c___|_\n",92);
+    printf (" /%c  )/%c/ ||  | )_)%c___,|))\n",92,92,92);
+    printf ("<  >      |(,,) )__)    |\n");
+    printf (" ||      /    %c)___)%c\n",92,92);
+    printf (" | %c____(      )___) )____\n",92);
+    printf ("  %c______(_______;;;)__;;;)\n\n",92);
+    printf ("Me cost%c un poco realizarlo por los c%cdigos ASCII\n",149,149);
+    printf ("--------------------------------------------------\n\n");
+    return 0;
+}
